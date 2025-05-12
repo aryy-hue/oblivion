3e7e94fdffb846981b7f4341ee19302dbc6ea3cc
aryy-hue <wardhana2451@gmail.com>
2025-05-13 00:10:45 +0700

    testing on index

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>About me</h1>
+    <p>Test xoxo.</p>
+    <NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template >
+  <div class="min-h-screen bg-base text-white flex items-center justify-center">
+    <h1 class="font-mono">Welcome fellas, <br> maybe you are not looking for this but i'd like to share something</h1>
+  </div>
+</template>
